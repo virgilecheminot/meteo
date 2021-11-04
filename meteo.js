6c9206fcb768f25b2c5474a541619ca64b4d3e91
Virgile Cheminot <virgile.cheminot@gmail.com>
2021-11-04 14:15:33 +0100

    position automatique

--- meteo.js
+++ meteo.js
@@ -1,57 +1,72 @@
-var callBackGetSuccess = function(data) {
+var callBackGetSuccess = function (data) {
     console.log("données api", data);
     // alert("Meteo temp : " + data.main.temp)
-    document.getElementById("temp").innerHTML = "La température est de " + data.main.temp + "°C";
-    document.getElementById("ressenti").innerHTML = "Température ressentie : " + data.main.feels_like + "°C";
-    document.getElementById("nuage").style.opacity = data.clouds.all / 100
-    document.getElementById("cloud percentage").innerHTML = "Couverture nuageuse : " + data.clouds.all +"%";
-    var temp = data.main.feels_like
+    document.getElementById("temp").innerHTML =
+        "La température est de " + data.main.temp + "°C";
+    document.getElementById("ressenti").innerHTML =
+        "Température ressentie : " + data.main.feels_like + "°C";
+    document.getElementById("nuage").style.opacity = data.clouds.all / 100;
+    document.getElementById("cloud percentage").innerHTML =
+        "Couverture nuageuse : " + data.clouds.all + "%";
+    document.getElementById("ville").innerHTML =
+        data.name;
+    var temp = data.main.feels_like;
     // temp = document.getElementById("slider").value
     // document.getElementById("temp arbitraire").innerHTML = temp;
     var hueMax = 255;
     if (temp <= 17.5) {
-        var blue = (-hueMax/17.5)*temp + hueMax;
-        var green = (hueMax/17.5)*temp;
+        var blue = (-hueMax / 17.5) * temp + hueMax;
+        var green = (hueMax / 17.5) * temp;
         var red = 0;
     } else if (17.5 < temp && temp <= 26.25) {
         var blue = 0;
         var green = hueMax;
-        var red = (hueMax/(26.25-17.5))*temp - 2*hueMax;
+        var red = (hueMax / (26.25 - 17.5)) * temp - 2 * hueMax;
     } else if (26.25 < temp && temp <= 35) {
         var blue = 0;
-        var green = (-hueMax/(26.25-17.5))*temp + 4*hueMax;
+        var green = (-hueMax / (26.25 - 17.5)) * temp + 4 * hueMax;
         var red = hueMax;
-    };
-    var color = new String("rgb("+red+","+green+","+blue+")");
+    }
+    var color = new String("rgb(" + red + "," + green + "," + blue + ")");
     var bgVal = document.getElementsByTagName("body");
     bgVal[0].style.background = color;
-    document.getElementById("humidity").innerHTML = "Humidité : " + data.main.humidity + "%";
-}
-
-// function buttonClickGET() {
-//     var queryLoc = document.getElementById("queryLoc").value;
-
-//     var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=45588b05eb419a5ff7319f7d44a6ca74&units=metric"
-//     $.get(url, callBackGetSuccess).done(function() {
-//         // alert("second success");
-//     })
-//     .fail(function() {
-//         alert("error");
-//     })
-//     .always(function() {
-//         // alert("finished");
-//     });
-// }
+    document.getElementById("humidity").innerHTML =
+        "Humidité : " + data.main.humidity + "%";
+};
 
 function onloadfunc() {
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat=45.19119457575232&lon=5.765416217189155&appid=45588b05eb419a5ff7319f7d44a6ca74&units=metric&lang=fr"
-    $.get(url, callBackGetSuccess).done(function() {
-        // alert("second success");
-    })
-    .fail(function() {
-        alert("error");
-    })
-    .always(function() {
-        // alert("finished");
-    });
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+
+        var lat = crd.latitude;
+        var long = crd.longitude;
+        var url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=" +
+            lat +
+            "&lon=" +
+            long +
+            "&appid=45588b05eb419a5ff7319f7d44a6ca74&units=metric&lang=fr";
+        $.get(url, callBackGetSuccess)
+            .done(function () {
+                // alert("second success");
+            })
+            .fail(function () {
+                alert("error");
+            })
+            .always(function () {
+                // alert("finished");
+            });
+    }
+
+    function error(err) {
+        console.warn(`ERREUR (${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
 }
